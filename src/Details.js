e766d120bfc9d8e1514472acdddc6bba52e0e963
mitsurubil <mitsurubil@hotmail.com>
2019-08-21 15:48:52 +0100

    Removed comment lines

--- src/Details.js
+++ src/Details.js
@@ -14,30 +14,6 @@ function Details() {
       <h1>{welcomeMessage}</h1>
     </Link>
   );
-
-  // class Details extends React.Component {
-
-  //   constructor() {
-  //     super();
-  //     this.state = {
-  //       welcomeMessage: 'Hello, this will be the details page for each Movie & TV show :)'
-  //     };
-  //   }
-
-  //   componentDidMount() {
-  //     setTimeout(() => {
-  //       this.setState({
-  //         welcomeMessage: 'Coming soon! :)'
-  //       });
-  //     }, 3000);
-  //   }
-  //   render() {
-  //     return (
-  //       <Link to='/'>
-  //         <h1>{this.state.welcomeMessage}</h1>
-  //       </Link>
-  //     );
-  //   }
 }
 
 export default Details;
