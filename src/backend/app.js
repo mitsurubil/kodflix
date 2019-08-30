493dd6d06189d5797241fe5591a00f817a15a094
mitsurubil <mitsurubil@hotmail.com>
2019-08-30 21:47:56 +0100

    Added this as a root prop of package.json  "proxy": "http://localhost:3001"

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const shows = require('./shows')
 const app = express()
-const port = 3000
+const port = 3001
 
 app.get('/rest/shows', (req, res) => res.send(shows()))
 
